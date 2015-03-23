9a3b71e0ed86697bb74d63b8121da9d385d6275b
Shanald <Shanald.Kumar@gmail.com>
2015-03-23 16:42:08 -0700

    added more methods for StationService

--- ChargingStationFinderApp/src/com/google/gwt/chargingstationfinder/server/StationServiceImpl.java
+++ ChargingStationFinderApp/src/com/google/gwt/chargingstationfinder/server/StationServiceImpl.java
@@ -123,6 +123,54 @@ StationService {
 		    
 		  }
 	  
+	  public void removeFavouriteStation(String address) throws NotLoggedInException {
+		    checkLoggedIn();
+		    PersistenceManager pm = getPersistenceManager();
+		    try {
+		      long deleteCount = 0;
+		      Query q = pm.newQuery(FavouriteStations.class, "user == u");
+		      q.declareParameters("com.google.appengine.api.users.User u");
+		      List<Station> stations = (List<Station>) q.execute(getUser());
+		      for (Station station : stations) {
+		        if (address.equals(station.getAddress())) {
+		          deleteCount++;
+		          pm.deletePersistent(station);
+		        }
+		      }
+		      if (deleteCount != 1) {
+		        logger.log(Level.WARNING, "removeStock deleted "+deleteCount+" Stocks");
+		      }
+		    } finally {
+		      pm.close();
+		    }
+		  }
+	  
+	  public String[][] getFavouriteStations() throws NotLoggedInException {
+		    checkLoggedIn();
+		    PersistenceManager pm = getPersistenceManager();
+		    String[][] returnVal = null;
+		    try {
+		      Query q = pm.newQuery(FavouriteStations.class, "user==u");
+		      q.declareParameters("com.google.appengine.api.users.User u");
+		      List<Station> stations = (List<Station>) q.execute();
+		      int i=0;
+		      if (stations.size() == 0) return null;
+		      if (stations.get(0) == null) return null;
+		      returnVal = new String[stations.size()][4];
+		      for (Station station : stations) {
+		        	  returnVal[i][0] = Double.toString(station.getLatitude());
+		        	  returnVal[i][1] = Double.toString(station.getLongitude());
+		        	  returnVal[i][2] = station.getOperator();
+		        	  returnVal[i][3] = station.getAddress(); 
+		        	  i++;
+		          }
+		    } finally {
+		      pm.close();
+		      
+		    }
+		    return returnVal;
+		  }
+	  
 	 
 
 }
